c8f027f206ae47c98e15af7282d1f2b3fda8f100
FelipeLEopoldino <felipeleopoldino01@icloud.com>
2022-03-28 15:15:09 -0300

    primeiro

---
+++ Aula-01/ex1.js
@@ -0,0 +1,54 @@
+const animes = [
+  {
+    id: 1,
+    nome: "Dragon Ball",
+    autor: "Akira Toriyama",
+  },
+  {
+    id: 2,
+    nome: "Yu Yu Hakusho",
+    autor: "Yoshihiro Togashi",
+  },
+  {
+    id: 3,
+    nome: "Saint Seiya",
+    autor: "Masami Kurumada",
+  },
+  {
+    id: 4,
+    nome: "Dragon Ball Z",
+    autor: "Akira Toriyama",
+  },
+  {
+    id: 5,
+    nome: "Naruto",
+    autor: "Masashi Kishimoto",
+  },
+];
+
+//Retorne um novo array contendo apenas os animes que foram criados pelo autor 'Akira Toriyama'.
+//Usar for
+// function akiraToriyama (){
+//     let criador = []
+//     for(let i = 0 ; i < animes.length ; i++){
+//         if(animes[i].autor == "Akira Toriyama"){
+//             criador.push(animes[i])
+//         }
+//     }return criador
+// }
+
+//  console.log(akiraToriyama(animes))
+
+// Usar for...of
+
+function akiraToriyama2() {
+  for (let criador of animes) {
+    if (criador.autor === "Akira Toriyama") return criador;
+  }
+}
+
+console.log(akiraToriyama2(animes));
+
+// Usar filter()
+
+//2 - Fazendo uso do método filter(), uma vez encontrado as obras do mestre Akira Toriyama, substitua o nome do autor pelo seu.
